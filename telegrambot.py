6bf3091b9e032eecf927c330260cac1c430028ce
elDalvini <49565899+elDalvini@users.noreply.github.com>
2019-10-09 19:03:38 +0200

    weekend support

--- telegrambot.py
+++ telegrambot.py
@@ -14,14 +14,18 @@ mycursor = mydb.cursor()
 cDate = date.today()
 tom = cDate + datetime.timedelta(1)
 tomrrw = datetime.datetime.combine(tom,datetime.datetime.min.time())
-tom1 = cDate + datetime.timedelta(1)
+tom1 = cDate + datetime.timedelta(2)
 tomrrw1 = datetime.datetime.combine(tom1,datetime.datetime.min.time())
 
 query = 'SELECT time_start FROM eventsG2 WHERE time_start >\"'+str(tomrrw)+'\" AND time_start<\"'+str(tomrrw1)+'\" ORDER BY `eventsG2`.`time_start` ASC'
 
 mycursor.execute(query)
-firstt = mycursor.fetchall()[0][0]
-wakeup = firstt - datetime.timedelta(0,0,0,0,15,1)
+result = mycursor.fetchall()
+if result > 0:
 
-if datetime.datetime.now() >= (wakeup-datetime.timedelta(0,0,0,0,0,8)):
-    bot.send_message(chat_id = chatID, text = "Go to bed! yout have to get up tomorrow at "+str(wakeup.hour)+":"+str(wakeup.minute))
+	firstt = result[0][0]
+
+	wakeup = firstt - datetime.timedelta(0,0,0,0,15,1)
+
+	if datetime.datetime.now() >= (wakeup-datetime.timedelta(0,0,0,0,0,8)):
+		bot.send_message(chat_id = chatID, text = "Go to bed! yout have to get up tomorrow at "+str(wakeup.hour)+":"+str(wakeup.minute))
